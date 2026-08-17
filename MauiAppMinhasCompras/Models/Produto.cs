a88819d42128c5a8634b8573dd35f84c4a2ad343
Talita Lopes <talitabc.lopes@hotmail.com>
2026-08-17 10:30:26 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco {  get; set; }
+    }
+}
