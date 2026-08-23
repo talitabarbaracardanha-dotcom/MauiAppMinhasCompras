28e00f7882fae7e0ace8ccd465fac957a9bc057d
Talita Lopes <talitabc.lopes@hotmail.com>
2026-08-23 20:04:06 -0300

    add  comandos no app

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppMinhasCompras.Helpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db 
+        {
+            get 
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            
+            }
+        
+        }
         public App()
         {
-            InitializeComponent();x
+            InitializeComponent();
 
             //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
